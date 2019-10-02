abd3b519549f66a507298c34780f01ac341d04eb
emilio barrera <barrera_emilio@hotmail.com>
2019-10-02 13:54:18 +0100

    Add project files.

---
+++ BindingEventsParametes/Classes/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BindingEventsParametes.Classes
+{
+    public class TaskItem
+    {
+        public string TaskDescription { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
